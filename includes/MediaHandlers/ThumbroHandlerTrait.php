617e5d9d7c1ae1e599a7515269d4745e7b130c4d
alistair3149 <alistair31494322@gmail.com>
2025-10-16 11:58:07 -0400

    fix: make sure that Thumbro replaces ThumbnailImage
    Fix the issue where Thumbro does not take over the rendering of the thumbnail HTML in subsquent visits

--- includes/MediaHandlers/ThumbroHandlerTrait.php
+++ includes/MediaHandlers/ThumbroHandlerTrait.php
@@ -3,6 +3,9 @@ declare( strict_types=1 );
 
 namespace MediaWiki\Extension\Thumbro\MediaHandlers;
 
+use MediaWiki\Extension\Thumbro\ThumbroThumbnailImage;
+use TransformParameterError;
+
 trait ThumbroHandlerTrait {
 	/**
 	 * @inheritDoc
@@ -16,5 +19,60 @@ trait ThumbroHandlerTrait {
 	 */
 	public function getThumbType( $ext, $mime, $params = null ) {
 		return [ 'webp', 'image/webp' ];
+	}
+
+	/**
+	 * We need to override this method to return a ThumbroThumbnailImage instance.
+	 * THe transform later flag can happen before the onBitmapHandlerTransform hook
+	 *
+	 * @see TransformationalImageHandler::doTransform
+	 *
+	 * @inheritDoc
+	 */
+	public function doTransform( $image, $dstPath, $dstUrl, $params, $flags = 0 ) {
+		if ( !( $flags & parent::TRANSFORM_LATER ) ) {
+			return parent::doTransform( $image, $dstPath, $dstUrl, $params, $flags );
+		}
+
+		if ( !$this->normaliseParams( $image, $params ) ) {
+			return new TransformParameterError( $params );
+		}
+
+		wfDebug( __METHOD__ . ": Transforming later per flags." );
+		$newParams = [
+			'width' => $params['width'],
+			'height' => $params['height']
+		];
+		if ( isset( $params['quality'] ) ) {
+			$newParams['quality'] = $params['quality'];
+		}
+		if ( isset( $params['page'] ) && $params['page'] ) {
+			$newParams['page'] = $params['page'];
+		}
+
+		return new ThumbroThumbnailImage( $image, $dstUrl, false, $newParams );
+	}
+
+	/**
+	 * We need to override this method to return a ThumbroThumbnailImage instance.
+	 * Since this can happen before the onBitmapHandlerTransform hook
+	 *
+	 * @see TransformationalImageHandler::getClientScalingThumbnailImage
+	 *
+	 * @inheritDoc
+	 */
+	protected function getClientScalingThumbnailImage( $image, $scalerParams ) {
+		$params = [
+			'width' => $scalerParams['clientWidth'],
+			'height' => $scalerParams['clientHeight']
+		];
+
+		$url = $image->getUrl();
+		if ( isset( $scalerParams['isFilePageThumb'] ) && $scalerParams['isFilePageThumb'] ) {
+			// Use a versioned URL on file description pages
+			$url = $image->getFilePageThumbUrl( $url );
+		}
+
+		return new ThumbroThumbnailImage( $image, $url, null, $params );
 	}
 }
